17076cb243fb79cabbd8c3705eda950f13492ee4
tharani as <tharani as@LAPTOP-CFMUGL08>
2019-04-06 18:29:48 +0530

    see

--- Sample-Projects/src/main/java/main/SampleProject.java
+++ Sample-Projects/src/main/java/main/SampleProject.java
@@ -4,13 +4,13 @@ public class SampleProject {
 	//print  Hello world! in the console
 	public static String printHello() {
 		//print Hello world! in the console!
-		String c = "Hello World!";
+		String c ="Hello World!";
 		return c;
 	}
 	public static int add(int a,int b) {
 		int c = a+b;
 		return c;
-	}
+		}
 	public static void main(String args[]) {
 		System.out.println(printHello());
 		System.out.println(add(23,45));
